93e9475d60e03ba9b0b97df074f75fad113202e4
xschen <xs0040@gmail.com>
2017-07-26 12:01:51 +0800

    mst implemented using kruskal

---
+++ spec/Kruskal_spec.lua
@@ -0,0 +1,42 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: chen0
+-- Date: 26/7/2017
+-- Time: 11:52 AM
+-- To change this template use File | Settings | File Templates.
+--
+
+describe('KruskalMST', function()
+    it('should find the minimum spanning tree', function()
+        local mst = require('mst.KruskalMST').create()
+        local g = require('data.graph').create(8)
+        g:addEdge(0, 7, 0.16)
+        g:addEdge(2, 3, 0.17)
+        g:addEdge(1, 7, 0.19)
+        g:addEdge(0, 2, 0.26)
+        g:addEdge(5, 7, 0.28)
+        g:addEdge(1, 3, 0.29)
+        g:addEdge(1, 5, 0.32)
+        g:addEdge(2, 7, 0.34)
+        g:addEdge(4, 5, 0.35)
+        g:addEdge(1, 2, 0.36)
+        g:addEdge(4, 7, 0.37)
+        g:addEdge(0, 4, 0.38)
+        g:addEdge(6, 2, 0.4)
+        g:addEdge(3, 6, 0.52)
+        g:addEdge(6, 0, 0.58)
+        g:addEdge(6, 4, 0.93)
+
+        mst:run(g)
+
+        local path = mst.path
+
+        assert.equal(path:size(), g.V-1)
+        for i=0,path:size()-1 do
+            local e = path:get(i)
+            print(e:from() .. ' -> ' .. e:to() .. ' (' .. e.weight .. ')')
+        end
+
+    end)
+end)
+
--- src/mst/KruskalMST.lua
+++ src/mst/KruskalMST.lua
@@ -13,10 +13,58 @@ function KruskalMST.create()
     local s = {}
     setmetatable(s, KruskalMST)
 
+    s.marked = {}
+    s.path = require('data.list').create()
     return s
 end
 
 function KruskalMST:run(G)
+    self.marked = {}
+    self.path = require('data.list').create()
+    local pq = require('data.MinPQ').create(function(e1, e2)
+        return e1.weight - e2.weight
+    end)
 
+    for v = 0, G.V-1 do
+        self.marked[v] = false
+    end
+
+    local adj_s = G:adj(0)
+    self.marked[0] = true
+    for i = 0, adj_s:size()-1 do
+        local e = adj_s:get(i)
+        pq:add(e)
+    end
+
+    local uf = require('data.UnionFind').create(G.V)
+    while pq:isEmpty() == false and self.path:size() < G.V - 1 do
+        local e = pq:delMin()
+        local v = e:either()
+        local w = e:other(v)
+        if uf:connected(v, w) == false then
+            uf:union(v, w)
+            self.path:add(e)
+
+            if self.marked[v] == false then
+                self.marked[v] = true
+                local adj_v = G:adj(v)
+                for i = 0, adj_v:size()-1 do
+                    local e_v = adj_v:get(i)
+                    pq:add(e_v)
+                end
+            end
+
+            if self.marked[w] == false then
+                self.marked[w] = true
+                local adj_w = G:adj(w)
+                for i = 0, adj_w:size()-1 do
+                    local e_w = adj_w:get(i)
+                    pq:add(e_w)
+                end
+            end
+        end
+    end
 end
+
+return KruskalMST
 
